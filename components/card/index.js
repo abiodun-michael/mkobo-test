d4b77241291168c5ece7b8896f25b577727576ad
abiodun-michael <michaemedewase@gmail.com>
2022-09-14 22:08:48 +0100

    initial commit

--- components/card/index.js
+++ components/card/index.js
@@ -1,1 +1,12 @@
-export {default as Analytic} from './Analytic'
+import AnalyticCard from "./Analytic";
+
+const {Item} = AnalyticCard
+export const Analytic = ()=>{
+    return(
+        <AnalyticCard>
+            <Item title="Avalaible Balance" data={6390.68} type="primary"/>
+            <Item  title="Car Purchase Plan" data={6390.68} type="secondary" interest={20000}/>
+            <Item title="Birthday Plan" data={6390.68} type="tertiary" interest={20000}/>
+        </AnalyticCard>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,12 @@
-import { Layout,Analytic, Divider, Tabs } from "components"
-import { Invisible, Naira } from "icons"
+import { Layout,Analytic, Divider } from "components"
 
-
-const {Item} = Analytic
-const {Header} = Tabs
-const {HeaderItem} = Header
 const Index = ()=>{
 
     return(
         <>
         <h1 style={{marginBottom:22, fontSize:22, color:'var(--text-color)'}}>Hello Bennedict,</h1>
-        <Analytic>
-            <Item title="Avalaible Balance" data={6390.68} type="primary"/>
-            <Item  title="Car Purchase Plan" data={6390.68} type="secondary" interest={20000}/>
-            <Item title="Birthday Plan" data={6390.68} type="tertiary" interest={20000}/>
-        </Analytic>
+        <Analytic />
         <Divider style={{marginBottom:25}}/>
-        <Tabs>
-            <Header data={[]}/>
-
-        </Tabs>
         </>
     )
 }
